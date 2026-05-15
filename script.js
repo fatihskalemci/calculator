12c6faa15323758d18168cc2be623c84cd88b021
Fatih Kalemci <fatihkalemci96@gmail.com>
2026-05-15 20:47:49 +0300

    Add buttons and screen

---
+++ script.js
@@ -0,0 +1,48 @@
+// Elements
+
+const topDisplay = document.querySelector("#top");
+const bottomDisplay = document.querySelector("#bottom");
+
+// Functions
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            add(a,b)
+            break;
+        case "-":
+            subtract(a,b)
+            break;
+        case "*":
+            multiply(a,b)
+            break;
+        case "/":
+            divide(a,b)
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+
+
+
+
