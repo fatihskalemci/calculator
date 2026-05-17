69e7d167c36c80e9d1280189c81ed6ef539e7616
Fatih Kalemci <fatihkalemci96@gmail.com>
2026-05-17 15:31:36 +0300

    Finish calculator

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-// Elements
+// Variables
+
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+
+
+// Elements and Event Listeners
 
 const topDisplay = document.querySelector("#top");
 const bottomDisplay = document.querySelector("#bottom");
+
+const clearButton = document.querySelector("#clear")
+clearButton.addEventListener('click', e => {
+    if (bottomDisplay.textContent === "0") {
+        clearTopDisplay();
+    }
+    clearBottomDisplay();
+})
+
+const operandButtons = document.querySelectorAll(".operand");
+[...operandButtons].forEach(item => {
+    item.addEventListener('click', () => {
+        getNumber(item.value);
+    });
+})
+
+const operationButtons = document.querySelectorAll(".operation");
+[...operationButtons].forEach(item => {
+    item.addEventListener('click', () => {
+        getOperator(item.value);
+    });
+})
+
+const equalButton = document.querySelector("#equal");
+equalButton.addEventListener('click', () => {
+    topDisplay.textContent = bottomDisplay.textContent;
+    operand1 = operate(operator, operand1, operand2);
+    operand2 = "";
+    operator = "";
+    updateBottomDisplay()
+
+});
 
 // Functions
 
@@ -18,30 +57,80 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
+    if (b === 0) {return "NaN"} 
     return a/b;
 }
 
+function modulus(a,b) {
+    return a%b;
+}
+
 function operate(operator, a, b) {
+    a = +a;
+    b = +b;
     switch (operator) {
         case "+":
-            add(a,b)
+            return add(a+b)
             break;
         case "-":
-            subtract(a,b)
+            return subtract(a,b)
             break;
         case "*":
-            multiply(a,b)
+            return multiply(a,b)
             break;
         case "/":
-            divide(a,b)
+            return divide(a,b)
+            break;
+        case "%":
+            return modulus(a,b)
             break;
         default:
             break;
     }
 }
 
+function updateBottomDisplay() {
+    if (operand1 + operator + operand2 === "") {
+        bottomDisplay.textContent = 0;
+    } else {
+        bottomDisplay.textContent = operand1 + operator + operand2;
+    }
+}
+
+function getNumber(value) {
+    if (bottomDisplay.textContent.length >= 10) {return}
+
+    if (operator === "") {
+        operand1 += value.toString();
+    } else {
+        operand2 += value.toString();
+    }
+    updateBottomDisplay();
+}
+
+function getOperator(value) {
+    if (bottomDisplay.textContent.length >= 10) {return}
+
+    if (operand2 === "") {
+        operator = value.toString();
+    }
+    updateBottomDisplay();
+}
+
+function clearBottomDisplay() {
+    operand1 = "";
+    operand2 = "";
+    operator = "";
+    updateBottomDisplay();
+}
+
+function clearTopDisplay() {
+    topDisplay.textContent = "";
+}
 
 
+
+updateBottomDisplay()
 
 
 
